62519fe7be2dbc65431cd8dd20373374ca11981e
Aleksey Belchenkov <u608110@gmail.com>
2018-12-31 19:30:10 +0300

    Create UI

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.autor = author;
+        this.isbn = isbn;
+    }
+
+}
+// UI Class: Handle UI Tasks
+class UI {
+
+}
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
